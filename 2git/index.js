cae0638c18d5e541156b549c64ef6eb3f4ee7627
Alex-Drug576 <sasa.com630@gmail.com>
2022-02-22 21:16:16 +0200

    2

---
+++ 2git/index.js
@@ -0,0 +1,13 @@
+var userName = 'AlexDrug';
+var ageNum = '20';
+var textSay = 'Hello World';
+var result;
+
+result = userName + ' ' + ageNum + ' ' + textSay;
+console.log(result);
+
+console.log(' +"40" + +"2" - 42 ');
+console.log('"2" + 3 ** 2 - 29');
+console.log('3 ** 2/3 - 3');
+console.log('3 ** (9/3) - 27');
+console.log('undefined + 1 - ')
