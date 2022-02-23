f4642df701a09dfc58207bbb1dd98f885dafc7ea
Alex-Drug576 <sasa.com630@gmail.com>
2022-02-23 11:58:29 +0200

    2.1

--- 2git/index.js
+++ 2git/index.js
@@ -6,8 +6,20 @@ var result;
 result = userName + ' ' + ageNum + ' ' + textSay;
 console.log(result);
 
-console.log(' +"40" + +"2" - 42 ');
-console.log('"2" + 3 ** 2 - 29');
-console.log('3 ** 2/3 - 3');
-console.log('3 ** (9/3) - 27');
-console.log('undefined + 1 - ')
+console.log(' +"40" + +"2" - 42\n',
+    '"2" + 3 ** 2 - 29\n',
+    '3 ** 2/3 - 3\n',
+    '3 ** (9/3) - 27\n',
+    'undefined + 1 - NaN\n',
+    '+null + 10 - 10\n',
+    'null / 5 - 0\n',
+    '"hello" + null - "hellonull"\n',
+    '"hello" - undefined + " " - NaN \n',
+    '"30" + 3 - "3" - 300\n',
+    '10 % "5" - 0');
+var number = 5;
+number = 2 * ++number;
+console.log(number);
+number = 5;
+number = 2 * number++;
+console.log(number);
